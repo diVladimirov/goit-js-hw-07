2931383798989cc016aa292dc1b17939cb763359
Vadim Vladimirov <kurtonchik@gmail.com>
2021-10-31 10:58:44 +0200

    add manual code on key left/right

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,5 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 
 const galleryContainer = document.querySelector(".gallery");
 const cardsMarkup = createCardsMarkup(galleryItems);
@@ -10,14 +9,15 @@ galleryContainer.insertAdjacentHTML("beforeend", cardsMarkup);
 galleryContainer.addEventListener("click", onGalleryContainerClick);
 
 function createCardsMarkup(cards) {
-    return cards
-    .map(({preview, original, description}) => {
-        return `<div class="gallery__item">
+  return cards
+    .map(({ preview, original, description }, index) => {
+      return `<div class="gallery__item">
         <a class="gallery__link" href="${original}">
           <img
             class="gallery__image"
             src="${preview}"
             data-source="${original}"
+            data-index="${index}"
             alt="${description}"
           />
         </a>
@@ -26,18 +26,81 @@ function createCardsMarkup(cards) {
     .join("");
 }
 
+const options = {
+  onShow: () => {
+    window.addEventListener("keydown", pressKey);
+  },
+  onClose: () => {
+    window.removeEventListener("keydown", pressKey);
+  },
+};
+
+let currentIndex = 0;
+
+const instance = basicLightbox.create(
+  `
+    <img src="" width="800" height="600">
+`,
+  options
+);
+
 function onGalleryContainerClick(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-const instance = basicLightbox.create(`
-    <img src="${event.target.dataset.source}" width="800" height="600">
-`)
-instance.show()
-
-document.addEventListener("keydown", event => {
-  if (event.key === "Escape") {
-    instance.close();
+  if (event.target.nodeName !== "IMG") {
+    return;
   }
-});
+  currentIndex = Number(event.target.dataset.index);
+  setLargeImg(event.target.dataset.source);
+  instance.show();
+}
 
+function pressKey({ key }) {
+  // if (event.key === "Escape") {
+  //   instance.close();
+  // }
+  // if (event.key === "ArrowRight") {
+  //   console.log("right");
+  //   currentIndex += 1;
+  //   if (currentIndex >= galleryItems.length) {
+  //     currentIndex = 0;
+  //   }
+  //   instance.element().querySelector("img").src =
+  //     galleryItems[currentIndex].original;
+  // }
+  // if (event.key === "ArrowLeft") {
+  //   console.log("left");
+  //   currentIndex -= 1;
+  //   if (currentIndex < 0) {
+  //     currentIndex = galleryItems.length - 1;
+  //   }
+  //   instance.element().querySelector("img").src =
+  //     galleryItems[currentIndex].original;
+  // }
+
+  switch (key) {
+    case "ArrowRight":
+      currentIndex += 1;
+      if (currentIndex >= galleryItems.length) {
+        currentIndex = 0;
+      }
+      setLargeImg(galleryItems[currentIndex].original);
+      break;
+    case "ArrowLeft":
+      currentIndex -= 1;
+      if (currentIndex < 0) {
+        currentIndex = galleryItems.length - 1;
+      }
+      setLargeImg(galleryItems[currentIndex].original);
+      break;
+    case "Escape":
+      instance.close();
+      break;
+    default:
+      alert("Warn");
+  }
+}
+
+function setLargeImg(src) {
+  instance.element().querySelector("img").src = src;
 }
